578339c828f292fd422fc43516423ae219ab0ddc
john-reiner <johnreiner1990@gmail.com>
2021-02-02 13:07:21 -0500

    ability to create user and encripted password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
-
+    def create
+        user = User.create!(
+            username: params[:username],
+            password_digest: params[:password],
+        )
+        render json: {message: "Welcome #{user.username}"}
+    end
 end
