3780755b55ead51bfe1a8fe454a150c82d66faeb
john-reiner <johnreiner1990@gmail.com>
2021-02-03 13:48:40 -0500

    added create and index methods for goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,13 @@
 class GoalsController < ApplicationController
+    def index
+        goals = Goal.all 
+        render json: goals
+    end
+
+    def create
+        goal = Goal.create(
+            name: params[:name]
+        )
+        render json: goal
+    end
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,4 @@
 class Goal < ApplicationRecord
     belongs_to :user
+    validates :name, presence: true
 end
