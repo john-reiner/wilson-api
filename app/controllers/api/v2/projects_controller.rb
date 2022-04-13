f4c88a7c292bf025536e282741891ccd67334935
john-reiner <johnreiner1990@gmail.com>
2022-04-13 08:13:59 -0400

    project show

--- app/controllers/api/v2/projects_controller.rb
+++ app/controllers/api/v2/projects_controller.rb
@@ -9,7 +9,7 @@ class Api::V2::ProjectsController < ApplicationController
 
   # GET /projects/1
   def show
-    render json: @project
+    render json: {status: :ok, message: @project}
   end
 
   # POST /projects
